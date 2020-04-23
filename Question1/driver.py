1b8b85508a9e5431f58ed2ce2f3547aeb0b968b3
vishal <subhavishal@yahoo.co.in>
2020-04-23 17:31:41 +0530

    Needs Debug

--- Question1/driver.py
+++ Question1/driver.py
@@ -20,4 +20,22 @@ for img_id in range (15):
     rep_images[img_id] = rep_image
 for i in range(15):
     plt.subplot(5,3,i+1)
-    plt.imshow(rep_images[i],plt.axis('off'),cmap='gray')
+    plt.axis('off')
+    plt.imshow(rep_images[i],cmap='gray')
+correct_predictions = 0
+for i in range(15):
+    for j in range(10):
+        matrix = np.ndarray(shape=(64,64), dtype = 'float32')
+        matrix = utils.read_pgm('.\\Dataset_Question1\\'+str(i + 1)+'\\'+str(j+1)+'.pgm',matrix)
+        match = 1
+        minimum = 1000000007
+        for img_id in range(15):
+            image_distance = np.linalg.norm(matrix-rep_images[img_id],1)
+            if minimum > image_distance:
+                match = img_id + 1
+                minimum = image_distance
+        if match == i + 1:
+            correct_predictions += 1
+accuracy = (correct_predictions/150)*100
+print("Accuracy =")
+print(accuracy)
